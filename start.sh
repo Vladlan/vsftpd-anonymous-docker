9669630dc61ab4da07bc931c2df3bf892bcb3242
Vladlan <vladislav_lanskih@smartexlab.com>
2018-11-02 17:12:39 +0300

    changed config

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -eu
 
 docker rm -f vsftpd-docker || true
 docker build -t vsftpd-anonymous-upload-docker .
-docker run -d --rm --name=vsftpd-docker -p 32021:21 -p 32022-32041:32022-32041 -v FtpVolume:/var/lib/ftp/incoming vsftpd-anonymous-upload-docker
+docker run -it --rm --name=vsftpd-docker -p 32021:21 -p 32022-32041:32022-32041 -v FtpVolume:/var/lib/ftp/incoming vsftpd-anonymous-upload-docker
 # sleep 5
 
 # Note ftp returns exit code 0 even if an error occurred
